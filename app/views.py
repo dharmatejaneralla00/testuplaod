e2799d5fe630a3cc17f1a8fbbff083b8bc0ecdbe
dharmatejaneralla287 <dharmatejaneralla287@gmail.com>
2023-08-22 15:31:42 +0530

    a

--- app/views.py
+++ app/views.py
@@ -15,5 +15,7 @@ def upload(r):
     ean = barcode.get(name="code128",code='1234ABC',writer=ImageWriter())
     # code = ean('1234', writer=ImageWriter())
     # ean.save(os.path.join(BASE_DIR, 'templates/docx/generateddrs/barcode/1234'))
-    models.Model(name='1234',files='files/'+ean.save(filename="1234ABC")).save()
+    # c = ean.save(filename="1234ABC")
+    c = ean.save(os.path.join(BASE_DIR,'media/files/1234'))
+    models.Model(name='1234',files=c).save()
     return HttpResponse("Hi")

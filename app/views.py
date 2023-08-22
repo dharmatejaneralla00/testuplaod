8b1178f275f5223d36e66c1de1b01fb822bea835
dharmatejaneralla287 <dharmatejaneralla287@gmail.com>
2023-08-22 14:50:30 +0530

    a

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
+import os
+
+import barcode
+from barcode.writer import ImageWriter
+from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
+from uploadfilses.settings import BASE_DIR
+
 
 # Create your views here.
+
+def upload(r):
+    # ean = barcode.get_barcode_class('code128')
+    ean = barcode.get(name="code128",code='1234ABC',writer=ImageWriter())
+    # code = ean('1234', writer=ImageWriter())
+    # ean.save(os.path.join(BASE_DIR, 'templates/docx/generateddrs/barcode/1234'))
+    models.Model(name='1234',files=ean.save("1234ABC"))
+    return HttpResponse("Hi")
